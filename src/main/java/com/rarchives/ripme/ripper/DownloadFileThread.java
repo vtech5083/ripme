e97251350fccc41637521b14e552598ba89bfa2d
Vivek Dudhat <vv405080@dal.ca>
2024-07-17 20:41:23 -0300

    refactor: Improve run method

--- src/main/java/com/rarchives/ripme/ripper/DownloadFileThread.java
+++ src/main/java/com/rarchives/ripme/ripper/DownloadFileThread.java
@@ -10,6 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -56,126 +57,152 @@ public class DownloadFileThread extends Thread {
         this.cookies = cookies;
     }
 
-    /**
-     * Attempts to download the file. Retries as needed.
-     * Notifies observers upon completion/error/warn.
-     */
     public void run() {
+        if (!shouldDownload()) {
+            return;
+        }
+
+        int tries = 0;
+        boolean redirected = false;
+        URL urlToDownload = this.url;
+
+        while (tries <= this.retries) {
+            tries++;
+            try {
+                HttpURLConnection huc = setupConnection(urlToDownload);
+                int statusCode = huc.getResponseCode();
+
+                if (isRedirect(statusCode)) {
+                    urlToDownload = handleRedirect(huc, redirected);
+                    redirected = true;
+                    tries--;
+                    continue;
+                }
+
+                if (isClientError(statusCode)) {
+                    handleClientError(statusCode);
+                    return;
+                }
+
+                if (isServerError(statusCode)) {
+                    handleServerError(statusCode);
+                    continue;
+                }
+
+                if (isImgurNotFound(huc, urlToDownload)) {
+                    handleImgurNotFound();
+                    return;
+                }
+
+                downloadFile(huc);
+                observer.downloadCompleted(url, saveAs);
+                logger.info("[+] Saved " + url + " as " + this.prettySaveAs);
+                return;
+
+            } catch (IOException e) {
+                handleDownloadException(e, tries);
+            }
+        }
+
+        handleExceededRetries();
+    }
+
+    private boolean shouldDownload() {
         try {
             observer.stopCheck();
         } catch (IOException e) {
             observer.downloadErrored(url, "Download interrupted");
-            return;
+            return false;
         }
+
+        if (saveAs.exists() && !Utils.getConfigBoolean("file.overwrite", false)) {
+            logger.info("[!] Skipping " + url + " -- file already exists: " + prettySaveAs);
+            observer.downloadExists(url, saveAs);
+            return false;
+        }
+
         if (saveAs.exists()) {
-            if (Utils.getConfigBoolean("file.overwrite", false)) {
-                logger.info("[!] Deleting existing file" + prettySaveAs);
-                saveAs.delete();
-            } else {
-                logger.info("[!] Skipping " + url + " -- file already exists: " + prettySaveAs);
-                observer.downloadExists(url, saveAs);
-                return;
-            }
+            logger.info("[!] Deleting existing file" + prettySaveAs);
+            saveAs.delete();
         }
 
-        URL urlToDownload = this.url;
-        boolean redirected = false;
-        int tries = 0; // Number of attempts to download
-        do {
-            tries += 1;
-            InputStream bis = null; OutputStream fos = null;
-            try {
-                logger.info("    Downloading file: " + urlToDownload + (tries > 0 ? " Retry #" + tries : ""));
-                observer.sendUpdate(STATUS.DOWNLOAD_STARTED, url.toExternalForm());
-
-                // Setup HTTP request
-                HttpURLConnection huc;
-                if (this.url.toString().startsWith("https")) {
-                    huc = (HttpsURLConnection) urlToDownload.openConnection();
-                }
-                else {
-                    huc = (HttpURLConnection) urlToDownload.openConnection();
-                }
-                huc.setInstanceFollowRedirects(true);
-                huc.setConnectTimeout(TIMEOUT);
-                huc.setRequestProperty("accept",  "*/*");
-                if (!referrer.equals("")) {
-                    huc.setRequestProperty("Referer", referrer); // Sic
-                }
-                huc.setRequestProperty("User-agent", AbstractRipper.USER_AGENT);
-                String cookie = "";
-                for (String key : cookies.keySet()) {
-                    if (!cookie.equals("")) {
-                        cookie += "; ";
-                    }
-                    cookie += key + "=" + cookies.get(key);
-                }
-                huc.setRequestProperty("Cookie", cookie);
-                logger.debug("Request properties: " + huc.getRequestProperties());
-                huc.connect();
-
-                int statusCode = huc.getResponseCode();
-                logger.debug("Status code: " + statusCode);
-                if (statusCode  / 100 == 3) { // 3xx Redirect
-                    if (!redirected) {
-                        // Don't increment retries on the first redirect
-                        tries--;
-                        redirected = true;
-                    }
-                    String location = huc.getHeaderField("Location");
-                    urlToDownload = new URL(location);
-                    // Throw exception so download can be retried
-                    throw new IOException("Redirect status code " + statusCode + " - redirect to " + location);
-                }
-                if (statusCode / 100 == 4) { // 4xx errors
-                    logger.error("[!] Non-retriable status code " + statusCode + " while downloading from " + url);
-                    observer.downloadErrored(url, "Non-retriable status code " + statusCode + " while downloading " + url.toExternalForm());
-                    return; // Not retriable, drop out.
-                }
-                if (statusCode / 100 == 5) { // 5xx errors
-                    observer.downloadErrored(url, "Retriable status code " + statusCode + " while downloading " + url.toExternalForm());
-                    // Throw exception so download can be retried
-                    throw new IOException("Retriable status code " + statusCode);
-                }
-                if (huc.getContentLength() == 503 && urlToDownload.getHost().endsWith("imgur.com")) {
-                    // Imgur image with 503 bytes is "404"
-                    logger.error("[!] Imgur image is 404 (503 bytes long): " + url);
-                    observer.downloadErrored(url, "Imgur image is 404: " + url.toExternalForm());
-                    return;
-                }
-
-                // Save file
-                bis = new BufferedInputStream(huc.getInputStream());
-                fos = new FileOutputStream(saveAs);
-                IOUtils.copy(bis, fos);
-                break; // Download successful: break out of infinite loop
-            } catch (HttpStatusException hse) {
-                logger.debug("HTTP status exception", hse);
-                logger.error("[!] HTTP status " + hse.getStatusCode() + " while downloading from " + urlToDownload);
-                if (hse.getStatusCode() == 404 && Utils.getConfigBoolean("errors.skip404", false)) {
-                    observer.downloadErrored(url, "HTTP status code " + hse.getStatusCode() + " while downloading " + url.toExternalForm());
-                    return;
-                }
-            } catch (IOException e) {
-                logger.debug("IOException", e);
-                logger.error("[!] Exception while downloading file: " + url + " - " + e.getMessage());
-            } finally {
-                // Close any open streams
-                try {
-                    if (bis != null) { bis.close(); }
-                } catch (IOException e) { }
-                try {
-                    if (fos != null) { fos.close(); }
-                } catch (IOException e) { }
-            }
-            if (tries > this.retries) {
-                logger.error("[!] Exceeded maximum retries (" + this.retries + ") for URL " + url);
-                observer.downloadErrored(url, "Failed to download " + url.toExternalForm());
-                return;
-            }
-        } while (true);
-        observer.downloadCompleted(url, saveAs);
-        logger.info("[+] Saved " + url + " as " + this.prettySaveAs);
+        return true;
     }
+
+    private HttpURLConnection setupConnection(URL urlToDownload) throws IOException {
+        HttpURLConnection huc = (HttpURLConnection) urlToDownload.openConnection();
+        huc.setInstanceFollowRedirects(true);
+        huc.setConnectTimeout(TIMEOUT);
+        huc.setRequestProperty("accept", "*/*");
+        if (!referrer.isEmpty()) {
+            huc.setRequestProperty("Referer", referrer);
+        }
+        huc.setRequestProperty("User-agent", AbstractRipper.USER_AGENT);
+        setCookies(huc);
+        huc.connect();
+        return huc;
+    }
+
+    private void setCookies(HttpURLConnection huc) {
+        String cookie = cookies.entrySet().stream()
+                .map(e -> e.getKey() + "=" + e.getValue())
+                .collect(Collectors.joining("; "));
+        huc.setRequestProperty("Cookie", cookie);
+    }
+
+    private boolean isRedirect(int statusCode) {
+        return statusCode / 100 == 3;
+    }
+
+    private URL handleRedirect(HttpURLConnection huc, boolean redirected) throws IOException {
+        String location = huc.getHeaderField("Location");
+        URL newUrl = new URL(location);
+        throw new IOException("Redirect status code " + huc.getResponseCode() + " - redirect to " + location);
+    }
+
+    private boolean isClientError(int statusCode) {
+        return statusCode / 100 == 4;
+    }
+
+    private void handleClientError(int statusCode) {
+        logger.error("[!] Non-retriable status code " + statusCode + " while downloading from " + url);
+        observer.downloadErrored(url, "Non-retriable status code " + statusCode + " while downloading " + url.toExternalForm());
+    }
+
+    private boolean isServerError(int statusCode) {
+        return statusCode / 100 == 5;
+    }
+
+    private void handleServerError(int statusCode) throws IOException {
+        observer.downloadErrored(url, "Retriable status code " + statusCode + " while downloading " + url.toExternalForm());
+        throw new IOException("Retriable status code " + statusCode);
+    }
+
+    private boolean isImgurNotFound(HttpURLConnection huc, URL urlToDownload) throws IOException {
+        return huc.getContentLength() == 503 && urlToDownload.getHost().endsWith("imgur.com");
+    }
+
+    private void handleImgurNotFound() {
+        logger.error("[!] Imgur image is 404 (503 bytes long): " + url);
+        observer.downloadErrored(url, "Imgur image is 404: " + url.toExternalForm());
+    }
+
+    private void downloadFile(HttpURLConnection huc) throws IOException {
+        try (InputStream bis = new BufferedInputStream(huc.getInputStream());
+             OutputStream fos = new FileOutputStream(saveAs)) {
+            IOUtils.copy(bis, fos);
+        }
+    }
+
+    private void handleDownloadException(IOException e, int tries) {
+        logger.debug("IOException", e);
+        logger.error("[!] Exception while downloading file: " + url + " - " + e.getMessage());
+    }
+
+    private void handleExceededRetries() {
+        logger.error("[!] Exceeded maximum retries (" + this.retries + ") for URL " + url);
+        observer.downloadErrored(url, "Failed to download " + url.toExternalForm());
+    }
+}
 
 }
